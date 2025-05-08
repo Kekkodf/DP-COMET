3c9d8863d4870a7e0cf8b1650c43c629c3bbd90a
kdf_7 <francesco.defaveri1999@gmail.com>
2025-05-08 08:59:49 +0200

    results DPMLM

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from src.utils import mylogger
-
 import os
 import sys
 import ir_datasets
+import pandas as pd
+
+from src.utils import mylogger
 
 if __name__ == '__main__':
-    logger = mylogger.create('test_logger')
+    logger = mylogger.create('main_logger')
+    df = pd.read_csv('data/tweet.csv')
+    df.columns = ['author', 'text']
+    #print number of unique authors and number of tweets
+    logger.info(f'Number of unique authors: {df["author"].nunique()}')
+    logger.info(f'Number of tweets: {df.shape[0]}')
+    #print average length of tweets
+    df['text_length'] = df['text'].apply(lambda x: len(x.split()))
+    logger.info(f'Average length of tweets: {df["text_length"].mean()}')
+    
     
